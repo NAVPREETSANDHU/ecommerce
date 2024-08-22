78abcafe02a76a42e8a78a727c817d72a8217801
unknown <12210994@cqumail.com>
2024-08-22 17:07:26 +1000

    Get products from databse

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,11 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
+import connectDB from './config/db.js';
 import products from './data/products.js';
+import connectDB from './config/db.js';
 const port = process.env.PORT || 8000;
+connectDB();
 
 const app = express();
 app.use(cors());
@@ -22,3 +25,6 @@ app.get('/api/products/:id', (req, res) => {
 });
 
 app.listen(port, () => console.log(`Server is running on Port ${port}`))
+app.listen(port, () =>
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+  );
