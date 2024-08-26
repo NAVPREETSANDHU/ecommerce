081dae2c1099efdd0e9941ca24dbc2caeebf95b0
cqumilankorangi <milan.korangi@cqumail.com>
2024-08-27 01:16:27 +1000

    connecting cart funcationality and redux

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,10 +4,11 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const password = encodeURIComponent(process.env.MONGO_PASSWORD);
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    // const password = encodeURIComponent(process.env.MONGO_PASSWORD)
+    const conn = await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true});
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
+    console.log(process.env.MONGO_URI)
     console.error(`Error: ${error.message}`);
     process.exit(1);
   }
